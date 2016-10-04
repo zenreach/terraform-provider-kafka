d38bc20ddc3e3c6b638c6bfafd605f3164e82224
Oliver Daff <oliverdaff@users.noreply.github.com>
2016-10-05 09:32:05 +1000

    updated tests

--- kafka_client_test.go
+++ kafka_client_test.go
@@ -5,8 +5,7 @@ import (
 )
 
 const (
-	validDescribeResponse =
-`Topic:file-imported	PartitionCount:12	ReplicationFactor:3	Configs:
+	validDescribeResponse = `Topic:file-imported	PartitionCount:12	ReplicationFactor:3	Configs:
 Topic: file-imported	Partition: 0	Leader: -1	Replicas: 0	Isr:
 Topic: file-imported	Partition: 1	Leader: -1	Replicas: 0	Isr:
 Topic: file-imported	Partition: 2	Leader: -1	Replicas: 0	Isr:
@@ -20,15 +19,14 @@ Topic: file-imported	Partition: 9	Leader: -1	Replicas: 0	Isr:
 Topic: file-imported	Partition: 10	Leader: -1	Replicas: 0	Isr:
 Topic: file-imported	Partition: 11	Leader: -1	Replicas: 0	Isr:`
 
-	shortDescribeResponse  = "Topic:file-imported	PartitionCount:12	ReplicationFactor:3	Configs:retention.ms=1457999337,cleanup.policy=compact"
+	shortDescribeResponse  = "Topic:file-imported	PartitionCount:12	ReplicationFactor:3	Configs:retention.ms=1457999337,cleanup.policy=compact,segment.ms=86400000,segment.bytes=10000"
 	shortDescribeResponse2 = "Topic:file-imported	PartitionCount:12	ReplicationFactor:3	Configs:retention.bytes=1023"
 
 	emptyDescribeResponse = ""
 
 	invalidDescribeResponse = "some unknown stuff"
 
-  noBrokersError =
-`Error while executing topic command : replication factor: 1 larger than available brokers: 0
+	noBrokersError = `Error while executing topic command : replication factor: 1 larger than available brokers: 0
 [2016-03-14 15:19:38,777] ERROR kafka.admin.AdminOperationException: replication factor: 1 larger than available brokers: 0
 	at kafka.admin.AdminUtils$.assignReplicasToBrokers(AdminUtils.scala:77)
 	at kafka.admin.AdminUtils$.createTopic(AdminUtils.scala:236)
@@ -37,8 +35,7 @@ Topic: file-imported	Partition: 11	Leader: -1	Replicas: 0	Isr:`
 	at kafka.admin.TopicCommand.main(TopicCommand.scala)
  (kafka.admin.TopicCommand$)`
 
-  topicExistsError =
-`Error while executing topic command : Topic "test" already exists.
+	topicExistsError = `Error while executing topic command : Topic "test" already exists.
 [2016-03-14 16:03:58,893] ERROR kafka.common.TopicExistsException: Topic "test" already exists.
 	at kafka.admin.AdminUtils$.createOrUpdateTopicPartitionAssignmentPathInZK(AdminUtils.scala:253)
 	at kafka.admin.AdminUtils$.createTopic(AdminUtils.scala:237)
@@ -47,8 +44,7 @@ Topic: file-imported	Partition: 11	Leader: -1	Replicas: 0	Isr:`
 	at kafka.admin.TopicCommand.main(TopicCommand.scala)
  (kafka.admin.TopicCommand$)`
 
-  errorWithWarnings =
-`WARNING: If partitions are increased for a topic that has a key, the partition logic or ordering of the messages will be affected
+	errorWithWarnings = `WARNING: If partitions are increased for a topic that has a key, the partition logic or ordering of the messages will be affected
 Error while executing topic command : The number of partitions for a topic can only be increased
 [2016-03-15 15:20:05,571] ERROR kafka.admin.AdminOperationException: The number of partitions for a topic can only be increased
 	at kafka.admin.AdminUtils$.addPartitions(AdminUtils.scala:119)
@@ -63,85 +59,106 @@ Error while executing topic command : The number of partitions for a topic can o
 )
 
 func TestKafkaManagingClient_topicInfo(t *testing.T) {
-  res, err := readTopicInfo(validDescribeResponse)
-  if err != nil { t.Fatal(err) }
-  assertInt   (t, "PartitionsCount",   res.PartitionsCount,   12)
-  assertInt   (t, "ReplicationFactor", res.ReplicationFactor, 3 )
-  assertInt64 (t, "RetentionBytes",    res.RetentionBytes,    -1)
-  assertInt64 (t, "RetentionMs",       res.RetentionMs,       -1)
-  assertString(t, "CleanupPolicy",     res.CleanupPolicy,     "")
+	res, err := readTopicInfo(validDescribeResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertInt(t, "PartitionsCount", res.PartitionsCount, 12)
+	assertInt(t, "ReplicationFactor", res.ReplicationFactor, 3)
+	assertInt64(t, "RetentionBytes", res.RetentionBytes, -1)
+	assertInt64(t, "RetentionMs", res.RetentionMs, -1)
+	assertString(t, "CleanupPolicy", res.CleanupPolicy, "")
+	assertInt64(t, "SegmentBytes", res.SegmentBytes, -1)
+	assertInt64(t, "SegmentMs", res.SegmentMs, -1)
 }
 
 func TestKafkaManagingClient_shortTopicInfo(t *testing.T) {
-  var res, err = readTopicInfo(shortDescribeResponse)
-  if err != nil { t.Fatal(err) }
-  assertInt   (t, "PartitionsCount",   res.PartitionsCount,   12)
-  assertInt   (t, "ReplicationFactor", res.ReplicationFactor, 3 )
-  assertInt64 (t, "RetentionBytes",    res.RetentionBytes,    -1)
-  assertInt64 (t, "RetentionMs",       res.RetentionMs,       1457999337)
-  assertString(t, "CleanupPolicy",     res.CleanupPolicy,     "compact")
+	var res, err = readTopicInfo(shortDescribeResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertInt(t, "PartitionsCount", res.PartitionsCount, 12)
+	assertInt(t, "ReplicationFactor", res.ReplicationFactor, 3)
+	assertInt64(t, "RetentionBytes", res.RetentionBytes, -1)
+	assertInt64(t, "RetentionMs", res.RetentionMs, 1457999337)
+	assertString(t, "CleanupPolicy", res.CleanupPolicy, "compact")
+	assertInt64(t, "SegmentBytes", res.SegmentBytes, 10000)
+	assertInt64(t, "SegmentMs", res.SegmentMs, 86400000)
 
-  res, err = readTopicInfo(shortDescribeResponse2)
-  if err != nil { t.Fatal(err) }
-  assertInt   (t, "PartitionsCount",   res.PartitionsCount,   12)
-  assertInt   (t, "ReplicationFactor", res.ReplicationFactor, 3 )
-  assertInt64 (t, "RetentionBytes",    res.RetentionBytes,    1023)
-  assertInt64 (t, "RetentionMs",       res.RetentionMs,       -1)
-  assertString(t, "CleanupPolicy",     res.CleanupPolicy,     "")
+	res, err = readTopicInfo(shortDescribeResponse2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertInt(t, "PartitionsCount", res.PartitionsCount, 12)
+	assertInt(t, "ReplicationFactor", res.ReplicationFactor, 3)
+	assertInt64(t, "RetentionBytes", res.RetentionBytes, 1023)
+	assertInt64(t, "RetentionMs", res.RetentionMs, -1)
+	assertString(t, "CleanupPolicy", res.CleanupPolicy, "")
+	assertInt64(t, "SegmentBytes", res.SegmentBytes, -1)
+	assertInt64(t, "SegmentMs", res.SegmentMs, -1)
 }
 
 func assertInt(t *testing.T, name string, value int, expected int) {
-  if (expected != value) {
-    t.Errorf("expected %s to be %d, but got %d", name, expected, value)
-  }
+	if expected != value {
+		t.Errorf("expected %s to be %d, but got %d", name, expected, value)
+	}
 }
 
 func assertInt64(t *testing.T, name string, value int64, expected int64) {
-  if (expected != value) {
-    t.Errorf("expected %s to be %d, but got %d", name, expected, value)
-  }
+	if expected != value {
+		t.Errorf("expected %s to be %d, but got %d", name, expected, value)
+	}
 }
 
 func assertString(t *testing.T, name string, value string, expected string) {
-  if (expected != value) {
-    t.Errorf("expected %s to be %s, but got %s", name, expected, value)
-  }
+	if expected != value {
+		t.Errorf("expected %s to be %s, but got %s", name, expected, value)
+	}
 }
 
 func TestKafkaManagingClient_emptyTopicInfo(t *testing.T) {
-  _, err := readTopicInfo(emptyDescribeResponse)
-  if err == nil { t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.") }
+	_, err := readTopicInfo(emptyDescribeResponse)
+	if err == nil {
+		t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.")
+	}
 }
 
 func TestKafkaManagingClient_invalidResponseTopicInfo(t *testing.T) {
-  _, err := readTopicInfo(invalidDescribeResponse)
-  if err == nil { t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.") }
+	_, err := readTopicInfo(invalidDescribeResponse)
+	if err == nil {
+		t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.")
+	}
 }
 
 func TestKafkaManagingClient_errorWithWarnings(t *testing.T) {
-  err := readError(errorWithWarnings)
+	err := readError(errorWithWarnings)
 
-  if err == nil { t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.") }
-  if err.Error() != "Error while executing topic command : The number of partitions for a topic can only be increased" {
-    t.Errorf("Unexpected error message: '%s'", err.Error())
-  }
+	if err == nil {
+		t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.")
+	}
+	if err.Error() != "Error while executing topic command : The number of partitions for a topic can only be increased" {
+		t.Errorf("Unexpected error message: '%s'", err.Error())
+	}
 }
 
 func TestKafkaManagingClient_parseNoBrokersError(t *testing.T) {
-  err := readError(noBrokersError)
+	err := readError(noBrokersError)
 
-  if err == nil { t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.") }
-  if err.Error() != "Error while executing topic command : replication factor: 1 larger than available brokers: 0" {
-    t.Errorf("Unexpected error message: '%s'", err.Error())
-  }
+	if err == nil {
+		t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.")
+	}
+	if err.Error() != "Error while executing topic command : replication factor: 1 larger than available brokers: 0" {
+		t.Errorf("Unexpected error message: '%s'", err.Error())
+	}
 }
 
-
 func TestKafkaManagingClient_parseTopicExistsError(t *testing.T) {
-  err := readError(topicExistsError)
+	err := readError(topicExistsError)
 
-  if err == nil { t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.") }
-  if err.Error() != "Error while executing topic command : Topic \"test\" already exists." {
-    t.Errorf("Unexpected error message: '%s'", err.Error())
-  }
+	if err == nil {
+		t.Fatal("Error is expected, but success found. Sometimes success is not what you are after.")
+	}
+	if err.Error() != "Error while executing topic command : Topic \"test\" already exists." {
+		t.Errorf("Unexpected error message: '%s'", err.Error())
+	}
 }
